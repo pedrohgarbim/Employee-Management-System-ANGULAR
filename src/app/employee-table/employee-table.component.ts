e6bc85a69ac74fda28829b8c1a199e41acd53bc6
pedrohgarbim <phg.garbim@gmail.com>
2024-09-04 00:25:36 -0300

    back e front

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../employee.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'employee-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.css'
 })
-export class EmployeeTableComponent {
+export class EmployeeTableComponent implements OnInit {
+  employees: Employee[] = [];
 
+  constructor(private employeeService: EmployeeService, private router: Router) { }
+
+  ngOnInit() {
+    this.employeeService.getEmployees().subscribe((data: Employee[]) => {
+      this.employees = data;
+      console.log(data);
+    });
+  }
 }
