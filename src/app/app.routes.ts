39759491763a06956e6bcedbcaa50ddf68aa4ece
pedrohgarbim <phg.garbim@gmail.com>
2024-09-04 15:07:42 -0300

    employee-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: EmployeeTableComponent },
+  { path: 'create', component: EmployeeFormComponent },
+  { path: 'employees', redirectTo: '', pathMatch: 'full' }
+];
